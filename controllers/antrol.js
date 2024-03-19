c6f058f80a04dd4e2a4927a3dee3e0ecc72fd1fc
fakhryhizballah <falehry88@gmail.com>
2024-03-20 01:00:50 +0700

    Add associations for maping_dokter_dpjpvclaim and maping_poli_bpjs in reg_periksa model

--- controllers/antrol.js
+++ controllers/antrol.js
@@ -27,7 +27,7 @@ module.exports = {
         if (dataPoli === null) {
             dataPoli = await maping_poli_bpjs.findAll();
             client.json.set('antrol:dataPoli', '$', dataPoli);
-            client.expire('antrol:dataPoli', 3600);
+            client.expire('antrol:dataPoli', 3600 * 24);
         }
 
         let startTime = new Date(); // Record start time
@@ -41,7 +41,7 @@ module.exports = {
                 }
             }
             client.json.set(`antrol:jadwalDr:${req.query.tanggal}`, '$', jadwal_Dr);
-            client.expire(`antrol:jadwalDr:${req.query.tanggal}`, 3600);
+            client.expire(`antrol:jadwalDr:${req.query.tanggal}`, 60 * 60 * 24);
             jadwalDr = jadwal_Dr;
         }
         let endTime = new Date();
@@ -52,27 +52,26 @@ module.exports = {
         // Output response time
         console.log('Response time jadwal dokter :', responseTime, 'milliseconds');
 
-        let pxTidakLengkap = [];
+        let tagg = 0;
         let dataAntrol = [];
         let logAntrol = [];
-        let noRM = [];
+        let dataAntrolCache 
         try {
             let sepTerbit = await axios.get(url_bpjs + `/api/bpjs/monitoring/kunjungan?from=${req.query.tanggal}&until=${req.query.tanggal}&pelayanan=2`);
             let dataSEP = sepTerbit.data.response.data;
+            // filter dataSEP yang poli nya IGD
             dataSEP = dataSEP.filter(item => item.poli !== 'IGD');
-            try {
-                let oldDataAntrol = fs.readFileSync('./cache/' + `antrol${req.query.tanggal}.json`);
-                if (oldDataAntrol) {
-                    dataAntrol = JSON.parse(oldDataAntrol);
-                    for (let x of dataAntrol) {
-                        dataSEP = dataSEP.filter(item => item.noRujukan !== x.nomorreferensi);
-                    }
-                }
-            } catch (error) {
-                console.log(error);
+            dataSEP = dataSEP.filter(item => item.poli !== 'HDL');
 
+            // if fs cache antrol.json exist
+            if (fs.existsSync('./cache/' + `antrol${req.query.tanggal}.json`)) {
+                // filter dari fs antrol.json
+                dataAntrolCache = fs.readFileSync('./cache/' + `antrol${req.query.tanggal}.json`, 'utf8');
+                dataAntrolCache = JSON.parse(dataAntrolCache);
+                let dataAntrolCacheMap = dataAntrolCache.map(item => item.nomorkartu);
+                dataSEP = dataSEP.filter(item => !dataAntrolCacheMap.includes(item.noKartu));
+                tagg = 1;
             }
-
             for (let x of dataSEP) {
                 let startTime = new Date();
                 let dataPx = await client.json.get(`antrol:dataPx:${x.noKartu}`, '$');
@@ -80,16 +79,10 @@ module.exports = {
                     dataPx = await axios.get(url_bpjs + `/api/bpjs/peserta/nokartu?nik=${x.noKartu}&tglSEP=${req.query.tanggal}`);
                     x.dataPx = dataPx.data.response;
                     client.json.set(`antrol:dataPx:${x.noKartu}`, '$', dataPx.data.response);
-                    client.expire(`antrol:dataPx:${x.noKartu}`, 3600);
+                    client.expire(`antrol:dataPx:${x.noKartu}`, 60 * 60 * 24 * 7);
                 } else {
                     console.log('cache');
                     x.dataPx = dataPx;
-                }
-
-                if (x.dataPx.peserta.mr.noMR === null) {
-                    pxTidakLengkap.push(x);
-                } else {
-                    noRM.push(x.dataPx.peserta.mr.noMR);
                 }
 
                 let endTime = new Date();
@@ -97,119 +90,117 @@ module.exports = {
                 // Output response time
                 console.log('Response time nokartu :', responseTime, 'milliseconds');
             }
+
             const reg_px = await reg_periksa.findAll({
                 where: {
                     [Op.and]: [
                         { tgl_registrasi: req.query.tanggal },
-                        { no_rkm_medis: noRM }
+                        { no_rkm_medis: dataSEP.map(item => item.dataPx.peserta.mr.noMR) }
                     ],
                 },
-                attributes: ['kd_dokter', 'jam_reg', 'tgl_registrasi', 'no_reg', 'stts_daftar', 'no_rawat', 'no_rkm_medis'],
-            });
-
-            if (reg_px === null) {
-                pxTidakLengkap.push(x);
-            }
-            // gabungkan dalam dataSEP bedasrakan noRM
-            for (let x of dataSEP) {
-                x.reg_px = reg_px.find(item => item.no_rkm_medis === x.dataPx.peserta.mr.noMR);
-            }
-            dataSEP = dataSEP.filter(item => item.reg_px !== null);
-
-            for (let x of dataSEP) {
-                // let reg_px = reg_px.find(item => item.no_rkm_medis === x.dataPx.peserta.mr.noMR);
-                try {
-
-                    let nomorSEP = await client.json.get(`antrol:nomorSEP:${x.noSep}`,);
-                    if (nomorSEP === null) {
-                        let data_nomorSEP = await axios.get(url_bpjs + `/api/bpjs/sep?noSEP=${x.noSep}`);
-                        nomorSEP = data_nomorSEP.data.response;
-                        client.json.set(`antrol:nomorSEP:${x.noSep}`, '$', nomorSEP);
-                        client.expire(`antrol:nomorSEP:${x.noSep}`, 3600 * 48);
-
+                include: [
+                    {
+                        model: maping_dokter_dpjpvclaim,
+                        as: 'maping_dokter_dpjpvclaim',
+                    },
+                    {
+                        model: maping_poli_bpjs,
+                        as: 'maping_poli_bpjs',
                     }
+                ],
+                attributes: ['kd_dokter', 'kd_poli', 'jam_reg', 'tgl_registrasi', 'no_reg', 'stts_daftar', 'no_rawat', 'no_rkm_medis'],
+            });
+            let combain = reg_px.map(item => ({
+                ...item.dataValues,
+                ...dataSEP.find(dataItem => dataItem.dataPx.peserta.mr.noMR === item.no_rkm_medis),
+            }));
 
-                    let data_poli = dataPoli.find(item => item.nm_poli_bpjs === nomorSEP.poli);
-                    let jadwaldrPoli = jadwalDr.find(x => x.kodedokter === parseInt(nomorSEP.dpjp.kdDPJP));
-                    let jamReg = new Date(x.reg_px.tgl_registrasi + ' ' + x.reg_px.jam_reg);
+            for (let x of combain) {
+                try {
+                    let jamReg = new Date(x.tgl_registrasi + ' ' + x.jam_reg);
                     jamReg.setTime(jamReg.getTime() + 10 * 60 * 1000);
                     let nohp = x.dataPx.peserta.mr.noTelepon;
-
+                    let jadwaldrPoli = jadwalDr.find(dataItem => dataItem.kodedokter === parseInt(x.maping_dokter_dpjpvclaim.kd_dokter_bpjs));
                     if (nohp.length >= 13) {
-                        console.log(nohp);
                         nohp = '000000000000';
                     }
                     if (nohp === null) {
                         nohp = '000000000000';
                     }
-
                     let addAntrol = {
-                        "kodebooking": x.reg_px.no_rawat,
+                        "kodebooking": x.no_rawat,
                         "jenispasien": "JKN",
-                        "nomorkartu": x.noKartu,
+                        "nomorkartu": x.dataPx.peserta.noKartu,
                         "nik": x.dataPx.peserta.nik,
                         "nohp": nohp,
-                        "kodepoli": data_poli.kd_poli_bpjs,
-                        "namapoli": nomorSEP.poli,
-                        "pasienbaru": x.reg_px.stts_daftar === 'Baru' ? 1 : 0,
-                        "norm": x.dataPx.peserta.mr.noMR,
+                        "kodepoli": x.maping_poli_bpjs.kd_poli_bpjs,
+                        "namapoli": x.maping_poli_bpjs.nm_poli_bpjs,
+                        "pasienbaru": x.stts_daftar === 'Baru' ? 1 : 0,
+                        "norm": x.no_rkm_medis,
                         "tanggalperiksa": req.query.tanggal,
-                        "kodedokter": parseInt(nomorSEP.dpjp.kdDPJP),
-                        "namadokter": nomorSEP.dpjp.nmDPJP,
+                        "kodedokter": parseInt(x.maping_dokter_dpjpvclaim.kd_dokter_bpjs),
+                        "namadokter": x.maping_dokter_dpjpvclaim.nm_dokter_bpjs,
                         "jampraktek": jadwaldrPoli.jadwal,
                         "jeniskunjungan": 3,
                         "nomorreferensi": x.noRujukan,
-                        "nomorantrean": `${x.poli}-${x.reg_px.no_reg}`,
-                        "angkaantrean": parseInt(x.reg_px.no_reg),
+                        "nomorantrean": `${x.poli}-${x.no_reg}`,
+                        "angkaantrean": parseInt(x.no_reg),
                         "estimasidilayani": jamReg.getTime(),
-                        "sisakuotajkn": jadwaldrPoli.kapasitaspasien - parseInt(x.reg_px.no_reg),
+                        "sisakuotajkn": jadwaldrPoli.kapasitaspasien - parseInt(x.no_reg),
                         "kuotajkn": jadwaldrPoli.kapasitaspasien,
-                        "sisakuotanonjkn": jadwaldrPoli.kapasitaspasien - parseInt(x.reg_px.no_reg),
+                        "sisakuotanonjkn": jadwaldrPoli.kapasitaspasien - parseInt(x.no_reg),
                         "kuotanonjkn": jadwaldrPoli.kapasitaspasien,
                         "keterangan": "Peserta harap 20 menit lebih awal guna pencatatan administrasi."
                     }
-                    let startTime = new Date();
 
-                    axios.post(url_bpjs + '/api/bpjs/antrean/add', addAntrol)
-                        .then(response => {
-                            if (response.data.metadata.code === 201) {
-                                logAntrol.push(addAntrol);
-                                logAntrol.push(response.data);
-                            }
-                            if (response.data.metadata.code === 200) {
-                                logAntrol.push(addAntrol);
-                                logAntrol.push(response.data);
-                                dataAntrol.push(addAntrol);
-                            }
-                            if (response.data.metadata.code === 208) {
-                                logAntrol.push(addAntrol);
-                                logAntrol.push(response.data);
-                                dataAntrol.push(addAntrol);
-                            }
-                        })
+                    let startTime = new Date();
+                    let postAntrol = await axios.post(url_bpjs + '/api/bpjs/antrean/add', addAntrol);
+                    if (postAntrol.data.metadata.code === 201) {
+                        logAntrol.push(addAntrol);
+                        logAntrol.push(postAntrol.data);
+                    }
+                    if (postAntrol.data.metadata.code === 200) {
+                        logAntrol.push(addAntrol);
+                        logAntrol.push(postAntrol.data);
+                        if (tagg === 1) {
+                            dataAntrolCache.push(addAntrol);
+                        } else {
+                            dataAntrol.push(addAntrol);
+                        }
+                    }
+                    if (postAntrol.data.metadata.code === 208) {
+                        logAntrol.push(addAntrol);
+                        logAntrol.push(postAntrol.data);
+                        if (tagg === 1) {
+                            dataAntrolCache.push(addAntrol);
+                        } else {
+                            dataAntrol.push(addAntrol);
+                        }
+                    }
                     let endTime = new Date();
+
+                    // Calculate response time
                     let responseTime = endTime - startTime;
+
                     // Output response time
-                    console.log('Response time add antrean :', responseTime, 'milliseconds');
+                    console.log('Response time antrean add :', responseTime, 'milliseconds');
                 } catch (error) {
                     console.error(error);
                 }
-
             }
 
-            fs.writeFileSync('./cache/' + `antrol${req.query.tanggal}.json`, JSON.stringify(dataAntrol));
+            if (tagg === 1) {
+                fs.writeFileSync('./cache/' + `antrol${req.query.tanggal}.json`, JSON.stringify(dataAntrolCache));
+            } else {
+                fs.writeFileSync('./cache/' + `antrol${req.query.tanggal}.json`, JSON.stringify(dataAntrol));
+            }
             fs.writeFileSync('./cache/' + `logAntrol${req.query.tanggal}.json`, JSON.stringify(logAntrol));
             return res.status(200).json({
                 status: false,
                 message: 'Success',
-                record: dataSEP.length,
-                datadataAntrol: dataAntrol.length,
-                dataAntrol,
-                pxTidakLengkap,
-                dataSEP,
-                // pxTidakLengkap: pxTidakLengkap.length,
-                // dataAntrol,
-                // data: dataSEP,
+                record: combain.length,
+                dataAntrolLength: dataAntrol.length,
+                data: dataAntrol,
             });
         } catch (error) {
             console.log(error);
