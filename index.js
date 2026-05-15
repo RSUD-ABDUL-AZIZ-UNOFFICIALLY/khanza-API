1f49f5db6b4197cf4516fe60bbc31eb3569ab763
fakhryhizballah <falehry88@gmail.com>
2026-05-15 23:47:45 +0700

    remove cors

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const path = require('path');
 
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
+// app.use(cors());
 const { createClient } = require('redis');
 const client = createClient({
     password: process.env.REDIS_PASSWORD,
